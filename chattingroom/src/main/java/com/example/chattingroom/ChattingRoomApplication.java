0e1bb5398df1e173768e1d59ad351c50b3569d8a
yuun-dev <yuun.dev@gmail.com>
2024-05-17 13:26:01 +0900

    first commit

---
+++ chattingroom/src/main/java/com/example/chattingroom/ChattingRoomApplication.java
@@ -0,0 +1,13 @@
+package com.example.chattingroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChattingRoomApplication.class, args);
+    }
+
+}
